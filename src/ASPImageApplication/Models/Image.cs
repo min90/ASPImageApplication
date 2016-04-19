8d0c708cfc2c09919373097e1a32f9cc2c94e548
min90 <jesso12@student.sdu.dk>
2016-04-19 13:35:43 +0200

    New properties for Image and Category

---
+++ src/ASPImageApplication/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPImageApplication.Models
+{
+    public class Image { public int ImageId { get; set; }
+    public string Title { get; set; }
+    public string Owner { get; set; }
+    public Category Category { get; set; }
+    public int CategoryId { get; set; } }
+}
